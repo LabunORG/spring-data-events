329478fce7c4ee1a90a717ac5e8c7b9e0419878f
Konstantin Labun <konstantin.labun@gmail.com>
2017-07-25 10:23:41 +0300

    fix missing @componentscan

--- src/main/java/org/labun/springframework/data/repository/events/SpringEventsAutoConfiguration.java
+++ src/main/java/org/labun/springframework/data/repository/events/SpringEventsAutoConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.repository.support.Repositories;
@@ -13,6 +14,7 @@ import org.springframework.data.repository.support.Repositories;
  */
 @Configuration
 @EnableAspectJAutoProxy
+@ComponentScan(basePackageClasses = SpringEventsAutoConfiguration.class)
 public class SpringEventsAutoConfiguration {
     @Bean
     public BeanPostProcessor repositoryEventHandlerAnnotationBeanPostProcessor() {
